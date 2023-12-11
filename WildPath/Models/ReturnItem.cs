85e83e1b39a2ddf3be17d21ef82f2a3fa9de47bf
cikuta <clayton.ikuta@gmail.com>
2023-12-11 15:04:57 -0800

    entity database

---
+++ WildPath/Models/ReturnItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WildPath.Models;
+
+public partial class ReturnItem
+{
+    public int FkReturnId { get; set; }
+
+    public int FkItemId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public virtual Item FkItem { get; set; } = null!;
+
+    public virtual Return FkReturn { get; set; } = null!;
+}
---
+++ WildPath/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WildPath.Models;
+
+public partial class Transaction
+{
+    public int PkTransactionId { get; set; }
+
+    public string FkEmail { get; set; } = null!;
+
+    public string Status { get; set; } = null!;
+
+    public DateTime PurchaseDate { get; set; }
+
+    public string ShippingMethod { get; set; } = null!;
+
+    public virtual Profile FkEmailNavigation { get; set; } = null!;
+
+    public virtual ICollection<Return> Returns { get; set; } = new List<Return>();
+}
