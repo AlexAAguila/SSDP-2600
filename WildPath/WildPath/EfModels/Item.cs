fcd6b269912cfc838d0c83aa65d5928f193c0f76
cikuta <clayton.ikuta@gmail.com>
2024-02-14 13:19:33 -0800

    Full crud for admin products

--- WildPath/WildPath/EfModels/Item.cs
+++ WildPath/WildPath/EfModels/Item.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WildPath.EfModels;
 
 public partial class Item
 {
+    [Display(Name = "Item ID")]
     public int PkItemId { get; set; }
+    [Display(Name = "Supplier")]
 
     public string Supplier { get; set; } = null!;
+    [Display(Name = "Item Name")]
 
     public string ItemName { get; set; } = null!;
-
+    [Display(Name = "Item Description")]
     public string ItemDetails { get; set; } = null!;
 
     public double Price { get; set; }
