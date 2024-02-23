0a246b0e4c947d500d2d8d5fd414729d4c488179
cikuta <clayton.ikuta@gmail.com>
2024-02-23 10:17:31 -0800

    imagestores

---
+++ WildPath/WildPath/EfModels/ImageStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WildPath.EfModels;
+
+public partial class ImageStore
+{
+    public int ImageId { get; set; }
+
+    public string FileName { get; set; } = null!;
+
+    public byte[] Image { get; set; } = null!;
+}
