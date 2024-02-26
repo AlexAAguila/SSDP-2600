a2be153067250f42b7b2ffec2aa06ebba439db16
cikuta <clayton.ikuta@gmail.com>
2024-02-26 10:52:23 -0800

    Moved ContactInfo into MyRegistedUser

--- WildPath/WildPath/Controllers/TransactionController.cs
+++ WildPath/WildPath/Controllers/TransactionController.cs
@@ -8,20 +8,18 @@ namespace WildPath.Controllers
     public class TransactionController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly ApplicationDbContext _context;
         private readonly IConfiguration _configuration;
         private readonly WildPathDbContext _wpdb;
-        public TransactionController(ILogger<HomeController> logger, ApplicationDbContext db, IConfiguration configuration, WildPathDbContext wpdb)
+        public TransactionController(ILogger<HomeController> logger, IConfiguration configuration, WildPathDbContext wpdb)
         {
             _logger = logger;
-            this._context = db;
             _configuration = configuration;
             _wpdb = wpdb;
         }
 
         public IActionResult Index()
         {
-            MyRegisteredUserRepo registeredUserRepo = new MyRegisteredUserRepo(_context);
+            MyRegisteredUserRepo registeredUserRepo = new MyRegisteredUserRepo(_wpdb);
 
 
             TransactionRepo transactionRepo = new TransactionRepo(_wpdb);
