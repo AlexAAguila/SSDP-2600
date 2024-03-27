13bea1eba38faadee291b00271b19301f76dfb13
cikuta <clayton.ikuta@gmail.com>
2024-03-27 11:38:00 -0700

    styling for admin products list

--- WildPath/WildPath/ViewModels/ProductVM.cs
+++ WildPath/WildPath/ViewModels/ProductVM.cs
@@ -6,11 +6,9 @@ namespace WildPath.ViewModels
     public class ProductVM
     {
         public WildPath.EfModels.Item Item { get; set; }
-        //public IEnumerable<WildPath.EfModels.ImageStore> ImageStore { get; set; }
         public ImageStore ImageStore { get; set; }
+
         [Display(Name = "Image")]
         public IFormFile ImageFile { get; set; }
-
-
     }
 }
