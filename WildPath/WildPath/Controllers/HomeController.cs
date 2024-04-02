220ce7abcab00ca2328cd2d774f4bb7bce55d50b
Alex Aguilar <alex_a_aguilar@outlook.com>
2024-04-02 02:26:48 -0700

    Will prefill on first time.

--- WildPath/WildPath/Controllers/HomeController.cs
+++ WildPath/WildPath/Controllers/HomeController.cs
@@ -60,6 +60,10 @@ namespace WildPath.Controllers
                                     .Select(u => u.Id)
                                     .FirstOrDefault();
                 CheckoutVM.PayerFullName = registeredUserRepo.GetFirstAndLastNameByEmail(User.Identity.Name);
+                if(!CheckoutVM.hasAddress)
+                {
+                    registeredUserRepo.AddFkAddress(CheckoutVM, userName);
+                }
             }
             transRepo.Add(CheckoutVM, userId);
 
